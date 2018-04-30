d52a6200e429726d5bad632950b44fef042462b0
jinyalin <349218364@qq.com>
2018-04-30 17:58:36 +0800

    bugfix

--- data/src/main/cpp/facet_id-lib.cpp
+++ data/src/main/cpp/facet_id-lib.cpp
@@ -1,6 +1,7 @@
 #include <jni.h>
 #include <string>
 #include <android/log.h>
+#include <string.h>
 
 #define LOG_TAG ("facet_id_check")
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
